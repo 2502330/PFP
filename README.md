aea6ed5426a4ddafdd1169d736d7d2f0a7e9152c
Jovan <2502330@sit.singaporetech.edu.sg>
2025-09-22 10:06:43 +0800

    IMDB builder and validator

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-## Instructions
+## Running the program
 
-1. `data` folder should contain `AFINN-en-165.txt`, `neg`, `pos`, `urls_neg.txt`, `urls_pos.txt`
+1. `data` folder should contain `neg`, `pos`,`AFINN-en-165.txt`, `imdb.json`, `urls_neg.txt`, `urls_pos.txt`
 2. Run `main.py`
-3. Check that `data/reviews.json` is built
-4. Check that `results/results.json` is built
+3. Check `results/results.json` for output
+
+## Building `imdb.json`
+
+1. Run `pip install -r requirements.txt`
+2. Run `imdb_builder.py`
+3. Check `data/imdb.json` for output
+4. Run `imdb_validator.py`
+    > Repeat until there are no more broken ids.
+    This is due to API rate limits by PyMovieDb.
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
 import json
 from pathlib import Path
-from concurrent.futures import ThreadPoolExecutor
+
 from sas import *
 
 def load_sentiments(file):
@@ -14,75 +13,24 @@ def load_sentiments(file):
                 sentiments[word] = int(score)
     return sentiments
 
-def sort_by_prefix(path):
-    return int(path.stem.split('_')[0])
-
-def load_review(file):
-    print(f'loading {file}')
-    return file.read_text('utf-8')
-
-def build_reviews_json():
-    print('building reviews.json')
-    # Load original review files in parallel
-    with ThreadPoolExecutor() as executor:
-        # sort_by_prefix ensures that files are loaded in the correct order
-        neg_files = sorted(Path('data/neg').glob('*.txt'), key=sort_by_prefix)
-        pos_files = sorted(Path('data/pos').glob('*.txt'), key=sort_by_prefix)
-
-        neg_reviews = list(executor.map(load_review, neg_files))
-        pos_reviews = list(executor.map(load_review, pos_files))
-
-        reviews = neg_reviews + pos_reviews
-
-    # Load ids from urls
-    neg_urls = Path('data/urls_neg.txt').read_text().splitlines()
-    pos_urls = Path('data/urls_pos.txt').read_text().splitlines()
-    urls = neg_urls + pos_urls
-    ids = [re.search(r'tt\d+', url).group() for url in urls if re.search(r'tt\d+', url)]
-
-    # Create a reviews dictionary with ids as keys and reviews as values
-    reviews_dict = {}
-    for id, review in zip(ids, reviews):
-        if id in reviews_dict:
-            reviews_dict[id].append(review)
-        else:
-            reviews_dict[id] = [review]
-
-    # Write to reviews.json
-    with open('data/reviews.json', 'w', encoding='utf-8') as f:
-        serialized = json.dumps(reviews_dict)
-        f.write(serialized)
-
-# Start timer to measure execution time
-t1 = time.perf_counter()
-
-# Load sentiment dictionary
+# Load sentiments dictionary
 sentiments = load_sentiments('data/AFINN-en-165.txt')
 
-# Build reviews.json if not built
-if not Path('data/reviews.json').is_file():
-    build_reviews_json()
-
-# Load reviews from reviews.json
-with open('data/reviews.json', 'r', encoding='utf-8') as f:
-    reviews = json.load(f)
-    reviews_count = sum(len(reviews) for reviews in reviews.values())
+# Load movies dictionary
+with open('data/imdb.json', 'r', encoding='utf-8') as f:
+    movies = json.load(f)
+    total_reviews = sum(len(movie['reviews']) for movie in movies.values())
 
 print(f'loaded {len(sentiments)} sentiments')
-print(f'loaded {len(reviews)} movies')
-print(f'loaded {reviews_count} reviews')
+print(f'loaded {len(movies)} movies')
+print(f'loaded {total_reviews} reviews')
 
 # Sentiment analysis
-results = {}
-for id in reviews.keys():
-    analysis = [analyze(review, sentiments) for review in reviews[id]]
-    results[id] = analysis
+for id in movies.keys():
+    analysis = [analyze(review, sentiments) for review in movies[id]['reviews']]
+    movies[id]['analysis'] = analysis
 
 # Write to results.json
 Path('results').mkdir(exist_ok=True)
 with open('results/results.json', 'w', encoding='utf-8') as f:
-    json.dump(results, f)
-
-# End timer to measure execution time
-t2 = time.perf_counter()
-print(f'time elapsed: {t2 - t1}s')
+    json.dump(movies, f, ensure_ascii=False, indent=4)
